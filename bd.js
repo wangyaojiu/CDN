6fe30cb65ff8ad6caf3093ab88f7b4b76cc314fe
wangyaojiu <69377161+wangyaojiu@users.noreply.github.com>
2020-11-05 10:25:55 +0800

    js css

---
+++ bd.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c4fa2dd15ff4d53e906ae5da8f07901d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
